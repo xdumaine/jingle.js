051ba4e34a14357b9798b128dde6aed828a50d95
Xander Dumaine <xander.dumaine@genesys.com>
2017-09-06 16:04:09 -0400

    patch to revert breaking change

--- test/filetransfer.js
+++ test/filetransfer.js
@@ -10,7 +10,7 @@ function setupSessionManagers() {
         jid: 'userB@example.com/bar'
     });
     var queueB = [];
-    
+
     jingleA.on('send', function (data) {
         data.from = jingleA.jid;
         queueB.push(data);
@@ -51,6 +51,7 @@ function getFile(){
 
 test('filetransfer', function (t) {
     var managers = setupSessionManagers();
+    var sendFile;
     managers[1].on('incoming', function (session) {
         t.pass('peer got incoming session');
         // FIXME: test it is a file transfer session
@@ -79,6 +80,6 @@ test('filetransfer', function (t) {
             t.pass('P2P connection established');
         }
     });
-    var sendFile = getFile();
+    sendFile = getFile();
     sess.start(sendFile);
 });
